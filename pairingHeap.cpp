692a28e7d7fd513d29d20ad707424a0616cbf5fc
Jasmine Katharina Pierre <jaspierre8304@gmail.com>
2026-01-28 20:16:54 -0600

    added function documentation and revised print

--- pairingHeap.cpp
+++ pairingHeap.cpp
@@ -17,99 +17,77 @@ public:
     }
 };
 
-bool isEmpty(Node* node) {
-    return node == nullptr;
-}
-//if prev points to node's parent, returns true
-bool isLeftmostChild(Node* node) {
-    if(node->prev != nullptr) {
-        if(node->prev->child == node) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    else {
-        cout << "Error: node->prev is nullptr";
-        return false;
-    }
-}
-
-Node* findLastChild(Node* parent) {
-    Node* child = parent->child;
-    //loop through to last child of parent 
-    while(child != nullptr) {
-        child = child->next;
-    }
-    return child;
-}
-
-Node* meld(Node* node1, Node* node2) {
-    if(isEmpty(node1)) {
-        return node2;
-    }
-    else if(isEmpty(node2)) {
-        return node1;
-    }
-    //make node2 a child of node1
-    else if(node1->key <= node2->key) {    
-        if(!isEmpty(node1->child)) { //node2 is not the first child of node1
-            node2->next = node1->child;
-            node1->child->prev = node2;
-        }
-        node2->prev = node1;
-        node1->child = node2;
-        return node1;
-    }
-    //make node1 a child of node2
-    else {
-        if(!isEmpty(node2->child)) { //node1 is not the first child of node2
-            node1->next = node2->child;
-            node2->child->prev = node1;
-        }
-        node1->prev = node2;
-        node2->child = node1;
-        return node2;
-    }
-}
-
-Node* mergePairs(Node* child) {
-    if(isEmpty(child)) {
-        return nullptr;
-    }
-    else if(isEmpty(child->next)) {
-        return child;
-    }
-    else {
-        Node* nextPair = child->next->next;
-        Node* second = child->next;
-
-        child->next = nullptr;
-        second->next = nullptr;
-        Node* melded = meld(child, second);
-        return meld(melded, mergePairs(nextPair));
-    }
-}
-
-
-    
-
-
 class PairingHeap {
 public:
     Node* root = nullptr;
 
+    //returns the node with the minimum key, which is the root
     Node* findMin() {
         return root;
     }
+
+    //checks if a pointer to a node is empty by comparing with null
+    bool isEmpty(Node* node) {
+        return node == nullptr;
+    }
+
+    //if prev points to node's parent, returns true
+    //if prev points to node's sibling, returns false
+    bool isLeftmostChild(Node* node) {
+        if(node->prev != nullptr) {
+            if(node->prev->child == node) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else { //this should only be true for the root and this function should never be called on the root
+            cout << "Error: node->prev is nullptr";
+            return false;
+        }
+    }
+
+    //combines two trees together by making the tree with the bigger root a child of the other tree
+    Node* meld(Node* node1, Node* node2) {
+        if(isEmpty(node1)) {
+            return node2;
+        }
+        else if(isEmpty(node2)) {
+            return node1;
+        }
+        //make node2 a child of node1
+        else if(node1->key <= node2->key) {    
+            if(!isEmpty(node1->child)) { //node2 is not the first child of node1
+                node2->next = node1->child;
+                node1->child->prev = node2;
+            }
+            node2->prev = node1;
+            node1->child = node2;
+            return node1;
+        }
+        //make node1 a child of node2
+        else {
+            if(!isEmpty(node2->child)) { //node1 is not the first child of node2
+                node1->next = node2->child;
+                node2->child->prev = node1;
+            }
+            node1->prev = node2;
+            node2->child = node1;
+            return node2;
+        }
+    }
+
     
+    
+    //creates a new node, adds it to the heap, and returns it
     Node* insert(int key) {
         Node* newNode = new Node(key);
         this->root = meld(this->root, newNode);
         return newNode;
     }
 
+    //extracts the node, replaces the key, and melds it back onto the heap
     void decreaseKey(Node* node, int newKey) {
 
         //remove subtree rooted at node
@@ -147,6 +125,27 @@ public:
             return r;
         }
     }
+    
+    //recursively merge root's children to find new min
+    Node* mergePairs(Node* child) {
+        if(isEmpty(child)) {
+            return nullptr;
+        }
+        else if(isEmpty(child->next)) {
+            return child;
+        }
+        else {
+            Node* nextPair = child->next->next;
+            Node* second = child->next;
+
+            child->next = nullptr;
+            second->next = nullptr;
+            Node* melded = meld(child, second);
+            return meld(melded, mergePairs(nextPair));
+        }
+    }
+
+
 
 
     //this print function is kind of a mess, but it at least reveals the heap's structure
@@ -154,11 +153,12 @@ public:
     //the children of the leftmost parent will be printed first
     //example: the fifth parent with a slash under it on a line has its children printed in the fifth group on the line below it
 
-    //parents should initially be empty
-    void printHeap(vector<Node*> parents) {
+    
+    void printHeap(vector<Node*> parents) { //parents should initially be empty
 
-        if(parents.size() == 0) { //initial call, prints root and recurses with root as only parent
-            cout << this->root->key << "\n";
+        if(parents.size() == 0) { //initial stack frame, prints the root and recurses with it as only parent
+            cout << this->root->key << " | \n";
+            cout << "/ \n";
             if(isEmpty(this->root->child)) {
                 return;
             }
@@ -201,11 +201,11 @@ public:
                 }
             }
             cout << line << "\n";
+            cout << ptrs << "\n";
             if(new_parents.size() == 0) { //base case, no more children to print
                 return;
             }
-            else {
-                cout << ptrs << "\n";
+            else { //otherwise recurse with parents to print their children
                 printHeap(new_parents);
             }
         }
@@ -214,10 +214,14 @@ public:
 
 int main() {
 
+    //testing params
     int n = 50;
     int e = 3;
     int lower_bound = 0;
     int upper_bound = 1000;
+
+    //used in recursive print function, initially empty
+    vector<Node*> parents;
 
     //init random number gen for testing
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
@@ -233,6 +237,7 @@ int main() {
         Node* minNode = pHeap.extractMin();
         cout << "Extracted min: " << minNode->key << "\n";
     }
-    vector<Node*> parents;
+
+    
     pHeap.printHeap(parents);
 }
